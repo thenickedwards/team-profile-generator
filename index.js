d5c6a4a52e9178598138257705aa75e42c779071
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-01-14 18:16:04 -0800

    All question arrays present, createTeamMember pushing engineer and intern data to allEmployees array.

--- index.js
+++ index.js
@@ -34,17 +34,93 @@ function createManager() {
     inquirer
         .prompt(managerQuestions)
         .then(function(data) {
-            console.log(data);
+            // console.log(data);
             allEmployees.push(data);
-            console.log(allEmployees);
+            // console.log("Below is the allEmployees array.")
+            // console.log(allEmployees);
             createTeamMember();
         })
-        // push mgr to allEmp array
-        // createTeamMember();
 }
 
-function createTeamMember() {
-    console.log("createTeamMember is running!")
+const engineerQuestions = [
+    {
+        name: "engineerName",
+        type: "input",
+        message: "What is the name of this engineer?"
+    },
+    {
+        name: "engineerId",
+        type: "input",
+        message: "What is this engineer's id?"
+    },
+    {
+        name: "engineerEmail",
+        type: "input",
+        message: "What is this engineer's email?"
+    },
+    {
+        name: "engineerGithub",
+        type: "input",
+        message: "What is this engineer's GitHub username?"
+    }
+];
+
+const internQuestions = [
+    {
+        name: "internName",
+        type: "input",
+        message: "What is the name of this intern?"
+    },
+    {
+        name: "internId",
+        type: "input",
+        message: "What is this intern's id?"
+    },
+    {
+        name: "internEmail",
+        type: "input",
+        message: "What is this intern's email?"
+    },
+    {
+        name: "internSchool",
+        type: "input",
+        message: "What school is this intern attending?"
+    }
+];
+
+async function createTeamMember() {
+    const response = await inquirer
+    .prompt([{
+            name: "continue",
+            type: "list",
+            message: "Which sort of team member would you like to add?",
+            choices: ["Engineer", "Intern", "I'm done! No more team members to add."]
+        }])
+
+    if (response.continue == 'Engineer') {
+        inquirer
+            .prompt(engineerQuestions)
+            .then(function(data) {
+                // console.log(data);
+                allEmployees.push(data);
+                // console.log("Below is the allEmployees array.")
+                // console.log(allEmployees);
+                createTeamMember();
+        })
+    } if (response.continue == 'Intern') {
+        inquirer
+            .prompt(internQuestions)
+            .then(function(data) {
+                // console.log(data);
+                allEmployees.push(data);
+                // console.log("Below is the allEmployees array.")
+                // console.log(allEmployees);
+                createTeamMember();
+        })
+    } else {
+        console.log("We're all done!");
+    }
+
     // Inq prompt do you want to add a TM
     // else, collect data
     // push to array
